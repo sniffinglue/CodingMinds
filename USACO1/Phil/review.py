6041bed1565c3ac43b00c2cd42e3bf1a57184c6e
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-09-21 12:41:49 -0700

    Update review.py
    adding questions

--- USACO1/Phil/review.py
+++ USACO1/Phil/review.py
@@ -1,2 +1,17 @@
-boo
-Block
+nums = [int(x) for x in input().split()]
+
+# takes an input 
+# split with spaces 
+# for loop
+# make each element an integer
+#  
+string_list = input().split()
+nums = []
+for x in string_list:
+    nums.append(int(x))
+
+print(nums)
+
+'''
+Write a program that reads in multiple integers separated by a space, and then store them inside a list.
+'''
