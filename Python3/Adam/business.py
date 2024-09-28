63d6adf210c777c4174e4c85c37efbfedf80fd66
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-09-28 15:28:46 -0700

    Adam's class project

---
+++ Python3/Adam/business.py
@@ -0,0 +1,32 @@
+import random
+class Business:
+  def __init__(self, name, price):
+    self.name = name
+    self.price = price
+  
+  def sell(self):
+    pass
+  
+  def buy(self):
+    pass
+  
+  def raisePrice(self):
+    pass
+  
+  def cutPrice(self):
+    pass
+
+def newStock():
+  pass
+
+Businesses = []
+# TODO: Implement Program
+# Greet the player
+while True:
+  # Menu
+  # If-elif-else chain
+  # for loop to make the stock drop unexpectedly
+  pass
+# say goodbye to the market 
+
+# END TODO
---
+++ Python3/Adam/dog.py
@@ -0,0 +1,9 @@
+class Dog():
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+
+dog1 = Dog("bob", 3, 'blue')
+print("Hi I have a dog what do you want to know")
+print(dog1.color)
