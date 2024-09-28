656b18f67f890e18ffb61b774af45dfae88747b7
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-09-28 12:24:18 -0700

    descriptive commit message

---
+++ Python3/Xingyi/s01.py
@@ -0,0 +1,11 @@
+class Cat:
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+
+fluffy = Cat("fluffy", 12, "orange")
+print(fluffy.age)
+
+boo = Cat("Boo")
+print(boo.name)
