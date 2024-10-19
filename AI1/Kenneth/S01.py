e75d1a710a07a3c002938a8863e04c292ce1cc68
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-10-18 18:40:54 -0700

    updating s01.py
    Uwala is better

--- AI1/Kenneth/S01.py
+++ AI1/Kenneth/S01.py
@@ -35,6 +35,19 @@ Kenneth = "Happy"
 if Kenneth == "Happy":
     print("Clap your hands")
 
+grade = 45
+
+if grade <= 100 and grade >= 90:
+    print("A")
+elif grade < 90 and grade >= 80:
+    print("B")
+elif grade < 80 and grade >= 70:
+    print("C")
+elif grade < 70 and grade >= 60:
+    print("D")
+else:
+    print("F")
+
 # loops 
     # while loop
 count = 0
@@ -44,4 +57,14 @@ while count <6:
 
     # for loops 
 for i in range(6):
+    print(i)
+
+# Lists 
+user_list = ["Adam", "Brianna", "Cole"]
+# adding 
+user_list.append("Kody")
+# remove
+user_list.remove("Adam")
+
+for i in user_list:
     print(i)
