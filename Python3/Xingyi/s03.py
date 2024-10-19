eccdb217edfb93c6d28965a597d29b765dcb93fb
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-10-19 11:21:58 -0700

    cool stuff with keys
    christina's cool drawing squares with keys project

--- Python3/Xingyi/s03.py
+++ Python3/Xingyi/s03.py
@@ -1,27 +1,27 @@
 import pygame
+import random
 
 pygame.init()
-clock = pygame.time.Clock() # The clock tracks how fast the game is running.
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((500, 500))
 
 while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      quit()
-  keystate = pygame.key.get_pressed()
-  #if up arrow, then draw a red box
-  if keystate[pygame.K_UP]:
-    rectangle = pygame.draw.rect(screen, (0, 255, 0), (390, 10, 100, 50))
-  #if down arrow, then draw a green box
-  elif keystate[pygame.K_DOWN]:
-    rectangle = pygame.draw.rect(screen, (255, 0, 0), (100, 10, 50, 50))
-  #if left arrow, then draw a blue box
-  elif keystate[pygame.K_LEFT]:
-    rectangle = pygame.draw.rect(screen, (0, 0, 255), (300, 200, 70, 50))
-  #if right arrow, then draw a white box
-  elif keystate[pygame.K_RIGHT]:
-    rectangle = pygame.draw.rect(screen, (255, 255, 255), (250, 40, 45, 50))
-  pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
 
-  clock.tick(60)
+        # Draw a rectangle based on a key press
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                # Draw a green rectangle at a random position
+                pygame.draw.rect(screen, (0, 255, 0), (random.randint(1, 400), random.randint(1, 450), 100, 50))
+            elif event.key == pygame.K_DOWN:
+                pygame.draw.rect(screen, (255, 0, 0), (random.randint(1, 400), random.randint(1, 450), 50, 50))
+            elif event.key == pygame.K_LEFT:
+                pygame.draw.rect(screen, (0, 0, 255), (random.randint(1, 400), random.randint(1, 450), 70, 50))
+            elif event.key == pygame.K_RIGHT:
+                pygame.draw.rect(screen, (255, 255, 255), (random.randint(1, 400), random.randint(1, 450), 45, 50))
+    
+    pygame.display.update()
+    clock.tick(60)
