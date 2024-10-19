070a589991a50a28cb3969aef6003c932ea4a074
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-10-18 18:16:21 -0700

    Update s.01.py
    Review Python with Kenneth

---
+++ AI1/Kenneth/S01.py
@@ -0,0 +1,19 @@
+# Python review
+# data types:
+    # Booleans -> True and False 
+    # Strings ->  Anything in "" , '', -> Quotations 
+    # Numbers ->   
+        # float -> 3.14159265
+        # integers -> 3, -5, 7
+# loops 
+    # while loop
+count = 0
+while count <10:
+    print(count)
+    count += 1
+    
+# if statements 
+Kenneth = "Happy"
+if Kenneth == "Happy":
+    print("Clap your hands")
+    # for loops 
