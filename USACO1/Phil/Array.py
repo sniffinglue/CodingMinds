7651d62ac8c11fdf3c18bedf657248cd1b1c0028
LENNYYYYYYY-S <unitygeneral2024+4@gmail.com>
2024-10-19 12:25:04 -0700

    fhfr
    boo

--- USACO1/Phil/Array.py
+++ USACO1/Phil/Array.py
@@ -2,5 +2,11 @@
 What are the indices of the elements "Apple", "Bread", and "Milk"?
 
 python
-food = ["Apple", "Shrimp", "Pineapple", "Bread", "Eggs", "Milk"]
 '''
+x=0
+food = ["Apple", "Shrimp", "Pineapple", "Bread", "Eggs", "Milk"]
+l = ["Apple","Bread","Milk"]
+for gay in food:
+    if gay in l:
+        print(x)
+    x+=1
