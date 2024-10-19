469d8f3fdc37709ea153c2527cf8e33153fecddd
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-10-18 18:35:34 -0700

    updating
    python review

--- AI1/Kenneth/S01.py
+++ AI1/Kenneth/S01.py
@@ -5,15 +5,43 @@
     # Numbers ->   
         # float -> 3.14159265
         # integers -> 3, -5, 7
-# loops 
-    # while loop
-count = 0
-while count <10:
-    print(count)
-    count += 1
-    
+
+# Logical Operator 
+# AND 
+# T T -> T
+# T F -> F
+# F F -> F
+
+# OR
+# T T -> T
+# T F -> T
+# F F -> F
+
+# NOT
+# T -> F
+# F -> T
+
+
+# Relational Operators
+# Less Than or Equal To (<=)
+# Less Than (<)
+# Equal To (==) - not to be confused with =, which is the assignment operator.
+# Not Equal To (!=)
+# Greater Than (>)
+# Greater Than or Equal To (>=)
+
 # if statements 
 Kenneth = "Happy"
 if Kenneth == "Happy":
     print("Clap your hands")
+
+# loops 
+    # while loop
+count = 0
+while count <6:
+    print(count)
+    count += 1
+
     # for loops 
+for i in range(6):
+    print(i)
